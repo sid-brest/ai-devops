cbf31542006ec52bc1c80e24dedcaae927dc167b
Dzmitry Struneuski <dzmitry_struneuski@epam.com>
2023-04-18 22:56:54 +0300

    sort and rename

---
+++ 1_bash_ip_challege.sh
@@ -0,0 +1,15 @@
+# Task1
+# Write a bash script that prompts the user to enter an IP address 
+# and then checks if the IP address matches the regular expression pattern.
+# If the IP address matches the pattern, the script outputs "The input IP address is valid.",
+# otherwise it outputs "The input IP address is not valid.
+
+#!/bin/bash
+echo "Please enter an IP address:"
+read ip_address
+regex_pattern="^([0-9]{1,3}\.){3}[0-9]{1,3}$"
+if [[ $ip_address =~ $regex_pattern ]]; then
+  echo "The input IP address is VALID."
+else
+  echo "The input IP address is NOT VALID."
+fi
